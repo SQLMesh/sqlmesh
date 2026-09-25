e6836f7ec6bba204d0b294e29a086327d452fe77
davem-bis <68955845+davem-bis@users.noreply.github.com>
2026-09-25 17:21:57 +0100

    Added whether or not the model is representative but not deployable to the missing interval check.
    Signed-off-by: davem-bis <68955845+davem-bis@users.noreply.github.com>

--- sqlmesh/core/scheduler.py
+++ sqlmesh/core/scheduler.py
@@ -180,11 +180,14 @@ class Scheduler:
             ignore_cron=ignore_cron,
             end_bounded=end_bounded,
         )
+
+        deployability_index = deployability_index or DeployabilityIndex.all_deployable()
+
         # Filtering snapshots after computing missing intervals because we need all snapshots in order
         # to correctly infer start dates.
         if selected_snapshots is not None:
             snapshots_to_intervals = {
-                s: i for s, i in snapshots_to_intervals.items() if s.name in selected_snapshots
+                s: i for s, i in snapshots_to_intervals.items() if s.name in selected_snapshots and not (deployability_index.is_representative(s) and not deployability_index.is_deployable(s))
             }
         return snapshots_to_intervals
 
